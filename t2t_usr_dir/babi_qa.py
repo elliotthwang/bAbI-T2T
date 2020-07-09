57431d2ed25e7371bdb88606ae03a481b786fa6a
elliotthwang <elliotthwang@gmail.com>
2020-07-09 12:17:08 +0800

    Update babi_qa.py

--- t2t_usr_dir/babi_qa.py
+++ t2t_usr_dir/babi_qa.py
@@ -383,8 +383,11 @@ class BabiQaSentence(problem.Problem):
     question_vocab_size = self._encoders[babi_qa.FeatureNames.QUESTION].vocab_size
     num_classes = self._encoders['targets'].vocab_size
 
-    p.modality = {babi_qa.FeatureNames.STORY: modalities.SymbolModality,
-                  babi_qa.FeatureNames.QUESTION: modalities.SymbolModality,
+   # p.modality = {babi_qa.FeatureNames.STORY: modalities.SymbolModality,
+    #              babi_qa.FeatureNames.QUESTION: modalities.SymbolModality,
+    p.modality = {babi_qa.FeatureNames.STORY: ModalityType.SYMBOL,
+                  babi_qa.FeatureNames.QUESTION: ModalityType.SYMBOL,
+
                   "targets": modalities.ClassLabelModality}
 
     p.vocab_size = {babi_qa.FeatureNames.STORY: story_vocab_size,
